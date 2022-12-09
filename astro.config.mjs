81c592d49c028ff659590dc927b814ee92ecff7f
clayton-schneider <schneiderc2016@gmail.com>
2022-12-08 21:15:06 -0500

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -7,6 +7,10 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  site: "https://sitegoeshere.com",
+  integrations: [tailwind(), image(), sitemap()],
 });
