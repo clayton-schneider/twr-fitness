7dcc0583e40d620dd7ee5af474844fc64c678f54
clayton-schneider <schneiderc2016@gmail.com>
2023-07-27 11:38:31 -0400

    added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,11 +18,15 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        oswald: ["Oswald", ...FONT_FAMILY_BASE],
+        sans: ["Open Sans", ...FONT_FAMILY_BASE],
+      },
       colors: {
         primary: "#271BF7",
         accent: "#F6AE2D",
         "text-light": "#FFFFFF",
-        "text-dark": "#000000",
+        "text-dark": "#2D2D2D",
       },
       padding: {
         edge: "2.5%",
