c7b46018110411a24344e368ca7f0e8eb5caf6b6
clayton-schneider <schneiderc2016@gmail.com>
2024-03-01 16:22:35 -0500

    plausible and partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,29 @@
 import { defineConfig, sharpImageService } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   build: {
-    format: "file",
+    format: "file"
   },
   experimental: {
-    assets: true,
+    assets: true
   },
   site: "https://twrfitness.net",
   image: {
-    service: sharpImageService(),
+    service: sharpImageService()
   },
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    react(),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), react(), sitemap(), partytown()]
 });
